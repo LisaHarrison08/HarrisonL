f4fd4e3ffe0a3b173ac5481ed51816eb896754a0
Lisa Harrison <lmharrison0810@gmail.com>
2022-03-15 20:50:56 -0400

    footer styled fa icons added and linked

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import './Footer.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
     return (
         <footer className="site-footer">
             {/* <div className="container"> */}
-                <div className="row">             
-                    <div className="col-4 text-center">
-                        <a className="btn btn-social-icon btn-instagram" href="http://instagram.com/"><i className="fas fa-github" /></a>{' '}
-                        <a className="btn btn-social-icon btn-facebook" href="http://www.facebook.com/"><i className="fa fa-facebook" /></a>{' '} 
-                    </div>
-                    <div className="col-4 text-center">
-                        <a role="button" className="btn btn-link" href="tel:+12065551234"><i className="fa fa-phone" /> 1-206-555-1234</a><br />
-                        <a role="button" className="btn btn-link" id="contact" href="mailto:notreal@notreal.co"><i className="fa fa-envelope-o" /> emailhere@gmail.co</a>
-                    </div>
+            <div className="row">
+                <div className="col text-center">
+                    <a href="mailto:l----@gmail.com" className="icon-link">
+                        <FontAwesomeIcon icon={faEnvelope} size="3x" />
+                    </a>
+                    <a href="https://github.com/" className="icon-link">
+                        <FontAwesomeIcon icon={faGithub} size="3x" />
+                    </a>
+                    <a href="https://www.linkedin.com/" className="icon-link">
+                        <FontAwesomeIcon icon={faLinkedin} size="3x" />
+                    </a>
                 </div>
+            </div>
             {/* </div> */}
         </footer>
     );
