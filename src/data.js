4a958e2b0a07a82ad23174521947856ff7707176
Lisa Harrison <lmharrison0810@gmail.com>
2022-03-23 23:20:29 -0400

    project images updated

--- src/data.js
+++ src/data.js
@@ -3,21 +3,21 @@ export default [
         id: 1,
         title: "Aurora",
         category: "Winery website",
-        image: "aurora1.png",
+        image: "aurora.png",
         url: ""
     },
     {
         id: 2,
         title: "Peak Hikes",
         category: "An adventure travel booking website",
-        image: "peak-hikes1.png",
+        image: "peak-hikes.png",
         url: ""
     },
     {
         id: 3,
         title: "Code Commit",
         category: "Coding commumity landing page",
-        image: "code-commit1.png",
+        image: "code-commit.png",
         url: "",
 
     },
@@ -25,16 +25,15 @@ export default [
         id: 4,
         title: "Covid Trends",
         category: "Data dashboard includes visualizations generated through plotly, d3 and tableau",
-        image: "covid-trends1.png",
+        image: "covid-trends.png",
         url: "https://covid-trends-us.herokuapp.com/index.html"
     },
     {
         id: 5,
         title: "Covid Predictions",
         category: "Dashboard contains charts reviewing COVID data and predicted trends across the US",
-        image: "covid-predictions1.png",
+        image: "covid-predictions.png",
         url: "https://covid-us.herokuapp.com/index.html"
 
-    }
-
+    } 
 ]
