09941454cc9440181f99981358fec27c80eecb09
Lisa Harrison <lmharrison0810@gmail.com>
2022-04-02 02:24:40 -0400

    corrected overflow

--- src/components/Tech.js
+++ src/components/Tech.js
@@ -7,26 +7,22 @@ function Tech() {
     const [items, setItems] = useState(Techdata)
 
     return (
-        <section id="tech">
-            <Container className="tech--content">
-                <Row>
-                    <Col md={4}>
-                        <h1>Tech I use:</h1>
-                        <hr />
-                    </Col>
-                    <Col md={8}>
-                        <p>There are so many choices, and I'm always interested in playing around with new technologies, but these are the ones that I dive into regularly</p>
-                    </Col>
-                </Row>
-
-                <Row xs={1} md={4}>
-                    {items.map((project, index) => (
-                        <TechCard project={project} key={project.id} />
-                    ))}
-                </Row>
-
-            </Container>
-        </section>
+        <Container fluid id="tech" className="tech--content">
+          <Row>
+                <Col md={4}>
+                    <h1>Tech I use:</h1>
+                    <hr />
+                </Col>
+                <Col md={8}>
+                    <p>There are so many choices, and I'm always interested in playing around with new technologies, but these are the ones that I dive into regularly</p>
+                </Col>
+            </Row>  
+            <Row xs={1} md={4}>
+                {items.map((project, index) => (
+                    <TechCard project={project} key={project.id} />
+                ))}
+            </Row>
+        </Container>
     )
 }
 
