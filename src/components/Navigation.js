321d0ac0b50c03ebfb16e9de8d14151ea94b4c3d
Lisa Harrison <lmharrison0810@gmail.com>
2022-04-02 01:50:17 -0400

    home nav link

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,7 +6,7 @@ function Navigation() {
     return (
         <nav>
             <Navbar className="justify-content-center" fixed="top" activeKey="/home">
-                <Nav.Link href="/home" className="nav-home">Home</Nav.Link>
+                <Nav.Link href="#home" className="nav-home">Home</Nav.Link>
                 <Nav.Link href="#about" className="nav-items">About</Nav.Link>
                 <Nav.Link href="#resume" className="nav-items">Resume</Nav.Link>
                 <Nav.Link href="#portfolio" className="nav-items">Portfolio</Nav.Link>
