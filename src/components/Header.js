fb98707206648ea078ff3fa12cf4791770ee0174
Lisa Harrison <lmharrison0810@gmail.com>
2022-05-21 19:54:44 -0400

    react-spring animation added to header text

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,24 @@
 import React from "react"
-import Fade from "react-reveal";
+// import Fade from "react-reveal";
+import { useSpring, animated } from 'react-spring';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub} from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faCode } from "@fortawesome/free-solid-svg-icons";
 import { Container, Row } from "react-bootstrap"
 import video from "../assets/video/aurora.mp4"
 
 
 const Header = () => {
+    const styles = useSpring({
+        from: { transform: "translateY(-100%)" },
+        to: [
+            { transform: "translateY(0%)" },
+            { transform: "translateY(50%)" },
+        ],
+        config: { duration: "1000" },
+        loop: false
+    })
+
     return (
         <header id="home">
             <Container fluid>
@@ -15,31 +26,24 @@ const Header = () => {
                     <video autoPlay loop muted>
                         <source src={video} type="video/mp4" />
                     </video>
-                    <div className="banner-text">
-                        <Fade bottom>
-                            <h1>Lisa Harrison</h1>
-                        </Fade>
-                        <Fade bottom duration={1000}>
-                            <h3>Full Stack Web Developer <br></br> Javascript | React | React Native<br></br>  Node.js | Express | MongoDB | PostgreSQL</h3>
-                        </Fade>
-                        <Fade>
-                            
-                        </Fade>
-                        < hr /> {/* <horizontal rule> used to seperate content */}
-                        <Fade bottom duration={3000}>
-                            <button style={{ marginRight: "20px" }}>
-                                <a href="#portfolio" className="btn project-btn">
-                                    <FontAwesomeIcon icon={faCode} size="xl" style={{ marginRight: "10px" }} />Projects
-                                </a>
-                            </button>
+                    <animated.div style={styles} className="banner-text">
+                        <h1>Lisa Harrison</h1>
+                        <h3>Full Stack Web Developer <br></br> Javascript | React | React Native<br></br>  Node.js | Express | MongoDB | PostgreSQL</h3>
 
-                            <button>
-                                <a href="https://github.com/" className="btn github-btn">
-                                    <FontAwesomeIcon icon={faGithub} size="xl" style={{ marginRight: "10px" }} />Github
-                                </a>
-                            </button>
-                        </Fade>
-                    </div>
+                        < hr /> {/* <horizontal rule> used to seperate content */}
+
+                        <button style={{ marginRight: "20px" }}>
+                            <a href="#portfolio" className="btn project-btn">
+                                <FontAwesomeIcon icon={faCode} size="xl" style={{ marginRight: "10px" }} />Projects
+                            </a>
+                        </button>
+
+                        <button>
+                            <a href="https://github.com/" className="btn github-btn">
+                                <FontAwesomeIcon icon={faGithub} size="xl" style={{ marginRight: "10px" }} />Github
+                            </a>
+                        </button>
+                    </animated.div>
                 </Row>
             </Container>
         </header>
